4e5d4c2f769a0cd962e9d07f05ee0bbb7da055d8
juaangarciaa05 <l>
2023-03-16 09:16:25 +0100

    cambios todos los dias

--- Day8/dictionaries.py
+++ Day8/dictionaries.py
@@ -1,3 +1,10 @@
 dog= {}
-dog= ['name':'Goku', 'color':'black and white', 'breed':'cocker', 'legs':'4', 'age': '18']
-student= {'fist_name': 'elena', 'last_name': 'aperador', 'gender': 'femenino', 'age' :'16', 'marital status' :'single', 'skills': 'karate', 'country': 'spain'}
+dog= {
+'name':'Goku' ,
+'color' ='black and white',
+'breed' ='cocker',
+'legs'= '4',
+'age' ='18' }
+dog=['name', 'color', 'breed', 'legs', 'age']
+student= {'fist_name': 'elena', 'last_name': 'aperador', 'gender': 'femenino', 'age' :'16', 'marital status' :'single', 'skills': 'karate', 'country': 'spain', 'city': 'cadiz'}
+print(len(student))
